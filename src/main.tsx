7e8ea47b77a2242f8d559658d6f59e90ecf178ab
NATANAEL RAZZOTTO <natanael8447@outlook.com>
2024-09-10 23:18:24 -0300

    feat: :art: Structure interfaces app

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App}  from './app'
+import './index.css';
+
+
+
+const rootElement = document.getElementById('root')
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+} else {
+  console.error("Root element not found")
+}
