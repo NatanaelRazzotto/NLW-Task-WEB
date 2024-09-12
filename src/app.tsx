053ff71f0e01100c011aea337f4e2f00360b8da6
NATANAEL RAZZOTTO <natanael8447@outlook.com>
2024-09-12 12:16:21 -0300

    feat: :construction_worker: structure data bind infos

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,47 @@ import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { EmptyGoals } from './components/empty-goals'
 import { Summary } from './components/summary'
+import { useEffect, useState } from 'react'
+type SummaryResponse = {
+  completed: number;
+  total: number;
+  goalsPeerDay: Record<string, {
+      id: string;
+      title: string;
+      completedAt: string;
+  }[]>;
+}
+
 export function App() {
+
+  const [summary, setSummary] = useState<SummaryResponse | null>(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary')
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      //console.log(data)
+      setSummary(data.summary)
+    })
+  },[] )
+
+  console.log(summary)
+
   return (
     <Dialog>
+
+      {summary && summary.total > 0 ? <Summary /> : <EmptyGoals />}
+
       {/* <EmptyGoals /> */}
-      <Summary />
+
+      {/* <pre> 
+        {
+          JSON.stringify(summary, null, 2)
+        }
+      </pre> */}
+       {/* <Summary />  */}
 
       <CreateGoal />
     </Dialog>
